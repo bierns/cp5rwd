af4efab8f7bc579eb3b3e4f1de24897967d9a92d
Gabriel Santos <iel.or.santoss@gmail.com>
2024-04-19 11:54:04 -0300

    aplicação da API

--- src/Components/Pages/Cadastro/Index.tsx
+++ src/Components/Pages/Cadastro/Index.tsx
@@ -1,33 +1,40 @@
 import './Style.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Cadastro = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+ 
+    const handleCadastro = async () => {
+        try {
+            const response = await axios.post('https://caiohalbert.bsite.net/api/user/newUser', {
+                username: username,
+                email: email,
+                passwordHash: password
+            });
+            console.log(response.data);
+            alert('Cadastro bem-sucedido! Você pode fazer login agora.');
+        } catch (error) {
+            console.error('Erro ao cadastrar:', error);
+            alert('Erro ao cadastrar. Por favor, tente novamente.'); 
+        }
+    };
+
     return (
         <div className="cadastro-container">
             <div className="card">
                 <h2>Cadastro</h2>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="name">Nome:</label>
-                        <input type="text" id="name" name="name" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="username">Nome de Usuário:</label>
-                        <input type="text" id="username" name="username" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Senha:</label>
-                        <input type="password" id="password" name="password" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="confirmPassword">Confirmar Senha:</label>
-                        <input type="password" id="confirmPassword" name="confirmPassword" />
-                    </div>
-                    <button type="submit">Cadastrar</button>
-                </form>
+                
+                <h1 className="cad">Cadastro</h1>
+                <input type="text" placeholder="Usuário" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button className="subimit" onClick={handleCadastro}>
+                    Cadastrar
+                </button>
+                
             </div>
         </div>
     );
--- src/Components/Pages/Login/Index.tsx
+++ src/Components/Pages/Login/Index.tsx
@@ -1,22 +1,53 @@
+import React, { useState } from "react";
 
-import './Style.css';
+import axios from "axios";
+
+import "./Style.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const url = "https://caiohalbert.bsite.net/api/user/login";
+
+  const handleLogin = async () => {
+    try {
+      console.log(email, password);
+
+      const response = await axios.post(url, {
+        email: email,
+        password: password,
+      });
+      console.log(response.data);
+      alert("Login bem-sucedido!");
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+      alert(
+        "Erro ao fazer login. Verifique suas credenciais e tente novamente."
+      );
+    }
+  };
+
     return (
         <div className="login-container">
             <div className="card">
                 <h2>Login</h2>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="username">Username:</label>
-                        <input type="text" id="username" name="username" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" name="password" />
-                    </div>
-                    <button type="submit">Login</button>
-                </form>
+                
+                <input
+          type="text"
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+                    <input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+                     <button className="button login" onClick={handleLogin}>
+          Logar
+        </button>
+                
             </div>
         </div>
     );
